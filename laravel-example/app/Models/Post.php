83dffe23d0fe6bd841e27df7014cb8bee2816c5a
JuanDavid888 <190215642+JuanDavid888@users.noreply.github.com>
2025-08-28 14:02:09 +0000

    feat: :construction: Validaciones en progreso
    Se están trabajando las referencias de tablas, falta crear la otra tabla a referencias

--- laravel-example/app/Models/Post.php
+++ laravel-example/app/Models/Post.php
@@ -4,12 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDelete;
 
 class Post extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDelete;
 
     protected $table = "posts";
 
-    protected $fillable = ['title', 'content', 'status'];
+    protected $fillable = [
+        'title', 'content', 'status',
+        'published_at', 'conver_image', 'tags', 'meta'
+    ];
+
+    protected $casts = [
+        'pubished_at' => 'datetime',
+        'tags' => 'array',
+        'meta' => 'meta'
+    ];
+
+    public function categories() {
+        // Tabla pivot post_category
+        return $this->belongsToMany(Category::class)->using(CategoryPost::class)->withTimestamps();
+    }
 }
